45b1fb85f9318cb892d0252cfcd25bdf75168198
Alberto Figueredoco <alberttolf@gmail.com>
2018-12-14 07:02:01 -0200

    Adicionados icones de cartas e layout pra pratica com baralhos

--- app/src/main/java/com/ifrj/tcc/lembre/activities/BaralhoActivity.java
+++ app/src/main/java/com/ifrj/tcc/lembre/activities/BaralhoActivity.java
@@ -1,7 +1,11 @@
 package com.ifrj.tcc.lembre.activities;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ifrj.tcc.lembre.R;
 
@@ -11,5 +15,45 @@ public class BaralhoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baralho);
+
+
+
+
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, MainActivity.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){ //Botão BACK padrão do android
+        startActivity(new Intent(this, MainActivity.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
+    }
+
+    private int getStatusBarHeight() {
+        int height;
+
+        Resources myResources = getResources();
+        int idStatusBarHeight = myResources.getIdentifier("status_bar_height", "dimen", "android");
+        if (idStatusBarHeight > 0) {
+            height = getResources().getDimensionPixelSize(idStatusBarHeight);
+        }else{
+            height = 0;
+            Toast.makeText(BaralhoActivity.this,
+                    "Resources NOT found",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        return height;
+    }
+
 }
